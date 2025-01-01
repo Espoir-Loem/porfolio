919d43cd7e6ca82a94610845a04bac56586bc841
Espoir-Loem <hopcy.forcy@gmail.com>
2025-01-01 23:03:13 +0100

    update

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -7,9 +7,9 @@ const HeroSection = () => {
     return (
         <section className=" flex flex-col gap-9 items-center px-xpage  pb-20 relative path-line">
 
-            <div style={{ zoom: 1.04 }} className="backdrop-blur-lg mb-3 p-6 size-80 rounded-full border bg-1/10">
+            <div style={{ zoom: 1.04 }} className="backdrop-blur-lg mb-3 p-5 size-80 rounded-full border bg-1/10">
                 <figure className=" relative size-full overflow-hidden rounded-full  shadow-[0_0_30px]  shadow-violet-1/20">
-                    <Image src="/espoir loém.png" alt="" fill />
+                    <Image src="/espoir loém 2.jpg" alt="" fill objectFit="cover" className="hover:scale-110 transition-all" />
                 </figure>
             </div>
             <Badge zoom={.75}>👋🏾 , Salut moi c’est</Badge>
