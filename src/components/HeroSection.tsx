27b0702e1d23cd371633f8e6de546d0390b2a200
Espoir-Loem <hopcy.forcy@gmail.com>
2025-01-02 11:10:11 +0100

    update hersection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,34 +5,46 @@ import Button, { NetworkIcons } from "./Button";
 const HeroSection = () => {
     const data: string[] = ["Développeur", "Web", "Mobile", "Designer", "UI", "UX", "DX"];
     return (
-        <section className="px-2 lg:px-xpage flex flex-col gap-9 items-center pb-20 relative path-line bg-red-200">
+        <section className=" flex flex-col gap-9 items-center pb-20 relative path-line ">
 
-            <div style={{ zoom: 1.04 }} className="backdrop-blur-lg mb-3 p-5 size-80 rounded-full border bg-1/10">
+            <div style={{ zoom: 1.04 }} className=" hidden md:block backdrop-blur-lg mb-3 p-5 size-80 rounded-full border bg-1/10">
                 <figure className=" relative size-full overflow-hidden rounded-full  shadow-[0_0_30px]  shadow-violet-1/20">
                     <Image src="/espoir loém 2.jpg" alt="" fill objectFit="cover" className="hover:scale-110 transition-all" />
                 </figure>
             </div>
-            <Badge zoom={.75}>👋🏾 , Salut moi c’est </Badge>
-            <h1 className="font-semibold text-5xl md:text-6xl">Espoir Loém</h1>
-            <div className="flex gap-3 flex-wrap  justify-center px-3">
-                {
-                    data.map((item) => (
-                        <Badge zoom={.8} key={item} active={item == "Développeur" || item == "Designer" ? true : false}>{item}</Badge>
-                    ))
-                }
-            </div>
-            <p className="px-1.5 md:px-20 text-[18px] md:text-[20px] text-center  opacity-75">
-                Chaque projet est une histoire 🌱, et je m’efforce d’en faire une œuvre mémorable 🎨. Je crée des solutions sur-mesure
-                🛠️ qui allient esthétique et efficacité ⚡, avec un œil attentif aux détails ✨. Ensemble, transformons vos idées
-                en expériences inoubliables 💫.
-            </p>
-            <div className="flex gap-9">
-                <div className="hidden md:block">
-                    <NetworkIcons  opacity={.75}/>
+
+            
+            <figure 
+                style={{
+                    backgroundImage: `linear-gradient(180deg, rgba(4, 1, 12, 0) 54.58%, rgba(4, 1, 12, 0.82) 78.95%, #04010C 100%), url('/espoir loém 2.jpg');`
+                    
+                }}
+                className=" bg-center md:hidden relative -z-1 h-[102dvh] w-full overflow-hidden  shadow-violet-1/20"
+            >
+            </figure>
+            <div className="-mt-80 md:mt-0 w-fit flex flex-col gap-9 items-center ">
+                <Badge zoom={.75}>👋🏾 , Salut moi c’est </Badge>
+                <h1 className="font-semibold text-5xl md:text-6xl">Espoir Loém</h1>
+                <div className=" flex gap-3 flex-wrap  justify-center px-3">
+                    {
+                        data.map((item) => (
+                            <Badge zoom={.8} key={item} active={item == "Développeur" || item == "Designer" ? true : false}>{item}</Badge>
+                        ))
+                    }
                 </div>
-                <Button>
-                    En savoir plus 👉🏾
-                </Button>
+                <p className="px-1.5 md:px-20 text-[18px] md:text-[20px] text-center  opacity-75">
+                    Chaque projet est une histoire 🌱, et je m’efforce d’en faire une œuvre mémorable 🎨. Je crée des solutions sur-mesure
+                    🛠️ qui allient esthétique et efficacité ⚡, avec un œil attentif aux détails ✨. Ensemble, transformons vos idées
+                    en expériences inoubliables 💫.
+                </p>
+                <div className="flex gap-9">
+                    <div className="hidden md:block">
+                        <NetworkIcons opacity={.75} />
+                    </div>
+                    <Button>
+                        En savoir plus 👉🏾
+                    </Button>
+                </div>
             </div>
 
 
