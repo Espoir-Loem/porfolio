2a4cfcc47a21741c96e6353c7d4efbc55ca1de24
Espoir-Loem <hopcy.forcy@gmail.com>
2025-01-04 01:01:37 +0100

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import { Icon } from "./Icon";
 
 const Header = () => {
     return (
-        <header className="px-6 lg:px-xpage  w-full flex justify-between   md:py-7 fixed md:sticky top-5 md:top-0 z-30">
+        <header className="px-4 lg:px-xpage  w-full flex justify-between   md:py-7 fixed md:sticky top-5 md:top-0 z-30">
             <Badge zoom={.8}>Qui suis-je 👇🏾 ?</Badge>
             <IconButton>
                 <Icon name="menu"/>
